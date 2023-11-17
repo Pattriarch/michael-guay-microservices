328c511b00af4d1913a2e22f72f9429b138da310
Daniil <pgelo298@mail.ru>
2023-11-18 00:02:45 +0300

    Добавил Guard для проверки авторизации между микросервисами, добавил начальный код для реализации Stripe

--- apps/auth/src/strategies/jwt.strategy.ts
+++ apps/auth/src/strategies/jwt.strategy.ts
@@ -1,6 +1,5 @@
 import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
-import { Request } from "express";
 import { ConfigService } from "@nestjs/config";
 import { UsersService } from "../users/users.service";
 import { ExtractJwt, Strategy } from "passport-jwt";
@@ -14,7 +13,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 	) {
 		super({
 			jwtFromRequest: ExtractJwt.fromExtractors([
-				(request: Request) => request?.cookies?.Authentication
+				(request: any) => request?.cookies?.Authentication || request?.Authentication
 			]),
 			secretOrKey: configService.get('JWT_SECRET')
 		});
---
+++ apps/payments/src/payments.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import Stripe from 'stripe';
+import { ConfigService } from "@nestjs/config";
+import { CreateChargeDto } from "./dto/create-charge.dto";
+
+@Injectable()
+export class PaymentsService {
+	constructor(private readonly configService: ConfigService) {
+	}
+
+	private readonly stripe = new Stripe(
+		this.configService.get('STRIPE_SECRET_KEY'),
+		{
+			apiVersion: '2023-10-16'
+		}
+	);
+
+	async createCharge({ card, amount }: CreateChargeDto) {
+		const paymentMethod = await this.stripe.paymentMethods.create({
+			type: 'card',
+			card
+		});
+
+		const paymentIntent = await this.stripe.paymentIntents.create({
+			payment_method: paymentMethod.id,
+			// takes cents
+			amount: amount * 100,
+			// immediately charge user
+			confirm: true,
+			payment_method_types: ['card'],
+			currency: 'usd',
+		});
+
+		return paymentIntent;
+	}
+}
