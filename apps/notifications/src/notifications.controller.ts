e934cf776768c8bf89acd1c388cb6bc1fff8bb08
daniilpereverzev <pgelo298@mail.ru>
2023-12-15 15:36:45 +0300

    Оптимизировал Dockerfile'ы

--- apps/notifications/src/notifications.controller.ts
+++ apps/notifications/src/notifications.controller.ts
@@ -10,6 +10,6 @@ export class NotificationsController {
   @UsePipes(new ValidationPipe())
   @EventPattern('notify_email')
   async notifyEmail(@Payload() data: NotifyEmailDto) {
-    this.notificationsService.notifyEmail(data);
+    await this.notificationsService.notifyEmail(data);
   }
 }
